a733f3a86110460d761531c03577cbe46a6fc269
YuliaS <ysolodukha@gmail.com>
2021-06-03 15:51:11 +0400

    deploy2

--- Server/ecosystem.config.js
+++ Server/ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
       host : '18.191.222.72',
       ref  : 'origin/main',
       repo : 'git@github.com:yulia-solo/Helper_Project.git',
-      path : '/home/ubunte/backend-helper',
+      path : '/home/ubuntu/backend-helper',
       env: {
         DB_NAME: 'helper_db',
         DB_HOST: 'helper-db.czeyex3idb1y.us-east-2.rds.amazonaws.com',
